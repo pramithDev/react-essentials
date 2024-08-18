b15bb4d75c885f40c9bb4ef44226e6fe34eb862d
pulze <malagodap2@gmail.com>
2024-08-18 22:18:43 +0530

    compoenent created

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
 import {CORE_CONCEPTS } from "./data.js";
-
-const reactDescriptions =['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <>
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-    </>
-  )
-}
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function App() {
   return (
@@ -44,6 +13,7 @@ function App() {
           <CoreConcept {...CORE_CONCEPTS[0]} />
           <CoreConcept {...CORE_CONCEPTS[1]} />
           <CoreConcept {...CORE_CONCEPTS[2]} />
+          <CoreConcept {...CORE_CONCEPTS[3]} />
         </ul>
         </section>
       </main>
