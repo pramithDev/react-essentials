1685800f180591f41f936279f80ce5255e2437c7
pulze <malagodap2@gmail.com>
2024-08-18 07:08:15 +0530

    Props changes added.

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   )
 }
@@ -41,21 +41,9 @@ function App() {
         <section id="core-concepts">
         <h2>Core Consepts</h2>
         <ul>
-          <CoreConcept
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-            image= {CORE_CONCEPTS[0].image}
-          />
-          <CoreConcept
-            title={CORE_CONCEPTS[1].title}
-            description={CORE_CONCEPTS[1].description}
-            image= {CORE_CONCEPTS[1].image}
-          />
-          <CoreConcept
-            title={CORE_CONCEPTS[2].title}
-            description={CORE_CONCEPTS[2].description}
-            image= {CORE_CONCEPTS[2].image}
-          />
+          <CoreConcept {...CORE_CONCEPTS[0]} />
+          <CoreConcept {...CORE_CONCEPTS[1]} />
+          <CoreConcept {...CORE_CONCEPTS[2]} />
         </ul>
         </section>
       </main>
