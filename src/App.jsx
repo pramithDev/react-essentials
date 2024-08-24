3ef4dce332a8611b4d49b457fa43c381ba551205
pulze <malagodap2@gmail.com>
2024-08-24 23:10:13 +0530

    click handler function added.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect() {
+    console.log("hello SELECTED");
+  }
   return (
     <div>
       <Header/>
@@ -20,11 +23,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Compoenents</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Compoenents</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
