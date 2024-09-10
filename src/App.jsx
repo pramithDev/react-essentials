8489747bd7dbc9d742bd1648cd948e1ed1064488
pulze <malagodap2@gmail.com>
2024-09-10 15:36:27 +0530

    Component  changes added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import {CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('please click a button'); 
+  const [ selectedTopic, setSelectedTopic ] = useState('components'); 
   function handleSelect(selectedButton) {
     // selectedButton => 'Components', 'JSX', 'props', 'state'
     // console.log(selectedButton);
     setSelectedTopic(selectedButton);
-    console.log(selectedTopic);
+    // console.log(selectedTopic);
   }
   return (
     <div>
@@ -28,12 +29,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('Components')}>Compoenents</TabButton>
-            <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Compoenents</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
