fabfa5c1dabbca70791f0b5623968666dfd1832a
pulze <malagodap2@gmail.com>
2024-11-21 22:24:50 +0530

    props changes added.

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import TabButton from "./TabButton";
 import Section from './Section.jsx';
 import { EXAMPLES } from '../data.js';
+import Tabs from './Tabs.jsx';
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -27,12 +28,28 @@ export default function Examples() {
   }
   return (
     <Section title="Examples" id="examples">
-      <menu>
-        <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Compoenents</TabButton>
-        <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
-        <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
-        <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
-      </menu>
+      <Tabs 
+      ButtonsContainer="menu"
+      buttons={
+        <>
+          <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Compoenents</TabButton>
+          <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
+          <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
+          <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
+        </>
+      }>
+        {!selectedTopic && <p>Please select a topic.</p>}
+        {selectedTopic && (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>)}
+      </Tabs>
       {/* {!selectedTopic ? <p>Please select a topic.</p> : null}
           {selectedTopic ? (
             <div id="tab-content">
@@ -44,17 +61,7 @@ export default function Examples() {
                 </code>
               </pre>
             </div>) : null} */}
-      {!selectedTopic && <p>Please select a topic.</p>}
-      {selectedTopic && (
-        <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>
-              {EXAMPLES[selectedTopic].code}
-            </code>
-          </pre>
-        </div>)}
+
       {/* {tabContent} */}
     </Section>
   )
